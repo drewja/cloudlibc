855f4aa2901e1b5b1769ee53a4c414d89458d2bf
Ed Schouten <ed@nuxi.nl>
2017-10-04 14:51:09 +0200

    Add compile time assertions for uv_buf_t layout.
    We should ensure that this structure remains identical to struct uio.

--- src/libc/uv/uv_buf_init.c
+++ src/libc/uv/uv_buf_init.c
@@ -3,7 +3,23 @@
 // This file is distributed under a 2-clause BSD license.
 // See the LICENSE file for details.
 
+#include <sys/uio.h>
+
+#include <assert.h>
+#include <stddef.h>
 #include <uv.h>
+
+static_assert(offsetof(struct iovec, iov_base) == offsetof(uv_buf_t, base),
+              "Offset mismatch");
+static_assert(sizeof(((struct iovec *)0)->iov_base) ==
+                  sizeof(((uv_buf_t *)0)->base),
+              "Size mismatch");
+static_assert(offsetof(struct iovec, iov_len) == offsetof(uv_buf_t, len),
+              "Offset mismatch");
+static_assert(sizeof(((struct iovec *)0)->iov_len) ==
+                  sizeof(((uv_buf_t *)0)->len),
+              "Size mismatch");
+static_assert(sizeof(struct iovec) == sizeof(uv_buf_t), "Size mismatch");
 
 uv_buf_t uv_buf_init(char *base, size_t len) {
   return (uv_buf_t){.base = base, .len = len};
